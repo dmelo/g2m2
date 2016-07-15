a0b49a98b0b406f7605abd98a5cab46704988ab9
Diogo Oliveira de Melo <dmelo87@gmail.com>
2016-07-15 17:34:28 -0300

    add newlines to disqus snipet

--- public/js/plugins/DisqusPlugin.js
+++ public/js/plugins/DisqusPlugin.js
@@ -13,20 +13,20 @@
 
 
     DisqusPlugin.prototype.postHtmlCalc = function (html) {
-        html += " \
-        <div id=\"disqus_thread\"></div>\
-            <script type=\"text/javascript\">\
-                /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */\
-                var disqus_shortname = '" + this.disqusId + "';\
-\
-                /* * * DON'T EDIT BELOW THIS LINE * * */\
-                (function() {\
-                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;\
-                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';\
-                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);\
-                })();\
-            </script>\
-            <noscript>Please enable JavaScript to view the <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>\
+        html += " \n\
+        <div id=\"disqus_thread\"></div>\n\
+            <script type=\"text/javascript\">\n\
+                /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */\n\
+                var disqus_shortname = '" + this.disqusId + "';\n\
+\n\
+                /* * * DON'T EDIT BELOW THIS LINE * * */\n\
+                (function() {\n\
+                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;\n\
+                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';\n\
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);\n\
+                })();\n\
+            </script>\n\
+            <noscript>Please enable JavaScript to view the <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>\n\
             <a href=\"http://disqus.com\" class=\"dsq-brlink\">comments powered by <span class=\"logo-disqus\">Disqus</span></a>";
         console.log("postHtmlCalc");
         console.log(html);
