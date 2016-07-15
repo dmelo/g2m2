490be4f82e32fda7082f3a75bb2e1bf8cfdf6723
Diogo Oliveira de Melo <dmelo87@gmail.com>
2016-07-15 17:48:39 -0300

    replace disqus universal code

--- public/js/plugins/DisqusPlugin.js
+++ public/js/plugins/DisqusPlugin.js
@@ -13,21 +13,21 @@
 
 
     DisqusPlugin.prototype.postHtmlCalc = function (html) {
-        html += " \n\
-        <div id=\"disqus_thread\"></div>\n\
-            <script type=\"text/javascript\">\n\
-                /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */\n\
-                var disqus_shortname = '" + this.disqusId + "';\n\
-\n\
-                /* * * DON'T EDIT BELOW THIS LINE * * */\n\
-                (function() {\n\
-                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;\n\
-                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';\n\
-                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);\n\
-                })();\n\
-            </script>\n\
-            <noscript>Please enable JavaScript to view the <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>\n\
-            <a href=\"http://disqus.com\" class=\"dsq-brlink\">comments powered by <span class=\"logo-disqus\">Disqus</span></a>";
+        html += "\n\
+<div id=\"disqus_thread\"></div>\n\
+<script>\n\
+    (function() {  // DON'T EDIT BELOW THIS LINE\n\
+        var d = document, s = d.createElement('script'),\n\
+            disqusId = '\" + this.disqusId + \"';\n\
+        \n\
+        s.src = '//' + disqusId + '.disqus.com/embed.js';\n\
+        \n\
+        s.setAttribute('data-timestamp', +new Date());\n\
+        (d.head || d.body).appendChild(s);\n\
+    })();\n\
+</script>\n\
+<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\" rel=\"nofollow\">comments powered by Disqus.</a></noscript>\n";
+
         console.log("postHtmlCalc");
         console.log(html);
 
