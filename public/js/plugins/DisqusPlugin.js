48c1755bf81b528ef6198bc72fbf00bc66874a27
Diogo Oliveira de Melo <dmelo87@gmail.com>
2016-07-15 17:50:41 -0300

    minor change

--- public/js/plugins/DisqusPlugin.js
+++ public/js/plugins/DisqusPlugin.js
@@ -13,20 +13,20 @@
 
 
     DisqusPlugin.prototype.postHtmlCalc = function (html) {
-        html += "\n\
-<div id=\"disqus_thread\"></div>\n\
-<script>\n\
-    (function() {  // DON'T EDIT BELOW THIS LINE\n\
-        var d = document, s = d.createElement('script'),\n\
-            disqusId = '\" + this.disqusId + \"';\n\
-        \n\
-        s.src = '//' + disqusId + '.disqus.com/embed.js';\n\
-        \n\
-        s.setAttribute('data-timestamp', +new Date());\n\
-        (d.head || d.body).appendChild(s);\n\
-    })();\n\
-</script>\n\
-<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\" rel=\"nofollow\">comments powered by Disqus.</a></noscript>\n";
+        html += "\\n\
+<div id=\"disqus_thread\"></div>\\n\
+<script>\\n\
+    (function() {  // DON'T EDIT BELOW THIS LINE\\n\
+        var d = document, s = d.createElement('script'),\\n\
+            disqusId = '\" + this.disqusId + \"';\\n\
+        \\n\
+        s.src = '//' + disqusId + '.disqus.com/embed.js';\\n\
+        \\n\
+        s.setAttribute('data-timestamp', +new Date());\\n\
+        (d.head || d.body).appendChild(s);\\n\
+    })();\\n\
+</script>\\n\
+<noscript>Please enable JavaScript to view the <a href=\"https://disqus.com/?ref_noscript\" rel=\"nofollow\">comments powered by Disqus.</a></noscript>\\n";
 
         console.log("postHtmlCalc");
         console.log(html);
